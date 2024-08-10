c37b5b24ade8c77242c72d1c9575676f7eca5fa8
shreyash377 <149990335+shreyash377@users.noreply.github.com>
2024-08-10 09:54:40 +0530

    Add files via upload

---
+++ Protfolio-Web/portfolio.js
@@ -0,0 +1,29 @@
+let web = document.getElementById("Yt-clone");
+web.addEventListener('click', (e) => {
+    let options = '';
+    let ref = window.open('index.html', '_blank', options);
+
+});
+let port = document.getElementById("Portfolio-web");
+port.addEventListener('click', (e) => {
+    let options = '';
+    let ref = window.open('portfoioWebsit.html', '_blank', options);
+
+});
+let clone = document.getElementById("College-web-prototype");
+clone.addEventListener('click', (e) => {
+    let options = '';
+    let ref = window.open('navbar.html', '_blank', options);
+
+});
+
+// const change=document.querySelector(".Left").innerText="Shreyash";
+// const paragraph=document.querySelector('p').innerText="I am Good"
+// const Allimage=document.querySelector("img")
+// const imageURL=[
+
+// ]
+
+
+
+
